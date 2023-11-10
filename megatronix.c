8edf36b328993c3b62342e96740476e74f63af0c
FiliusMahiae <sergio.mahiarello@gmail.com>
2023-11-10 18:38:24 +0100

    Se añade el parseo de las ordenes de acceso a RAM

--- megatronix.c
+++ megatronix.c
@@ -17,6 +17,7 @@ typedef struct {
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]); 
 void inicializarLinea(T_CACHE_LINE *linea);
 void VolcarCACHE(T_CACHE_LINE *tbl); 
+int getRam(unsigned int acceso);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque); 
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque); 
 void MostrarCACHE(T_CACHE_LINE *tbl);
@@ -33,8 +34,19 @@ int main(int argc, char **argv){
     MostrarCACHE(cache);
 
     lecturaArchivoBinario("CONTENTS_RAM.bin", Simul_RAM);
-}
 
+    FILE *accesos = NULL;
+    accesos = fopen("accesos_memoria.txt", "r");
+    
+    if(accesos==NULL){
+        printf("ERROR: archivo \"accesos_memoria.txt\" no encontrado\n");
+        exit(-1);
+    }
+
+    //getRam(0x22E);
+
+    return 0;
+}
 
 void inicializarLinea(T_CACHE_LINE *linea){
     linea->ETQ = 0xFF;
@@ -74,6 +86,34 @@ void lecturaArchivoBinario(char *nombreFichero, unsigned char *pSimul_RAM){
         pSimul_RAM[ramOcupada] = bitLeido;
         ramOcupada++;
     }
-    
+
     fclose(arch);
 }
+
+int getRam(unsigned int acceso){
+    int ETQ=0;
+    int palabra=0;
+    int linea=0;
+    int bloque=0;
+    ParsearDireccion(acceso,&ETQ,&palabra,&linea,&bloque);
+    //printf("%d\n",ETQ);
+    //printf("%d\n",palabra);
+    //printf("%d\n",bloque);
+    //parsea direccion
+    //se busca
+        //si fallo
+        //se trata
+        //se sale
+    
+        //si no hay fallo
+        //se añade al fichero
+        //Se sale
+}
+
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque){
+    *ETQ = (addr & 0b111110000000) >> 7;
+    *bloque = (addr & 0b000001110000) >> 4;
+    *palabra = (addr & 0b000000001111);
+    *linea = *bloque % NUM_FILAS;
+}
+
