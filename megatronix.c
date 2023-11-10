8dda50d3f38c831da981d648a01c20f21f565048
laura <lau.ap04@gmail.com>
2023-11-10 17:13:47 +0100

    lectura archivo binario

--- megatronix.c
+++ megatronix.c
@@ -3,6 +3,7 @@
 
 #define TAM_LINEA 16
 #define NUM_FILAS 8
+#define TAM_RAM 4096
 
 
 typedef struct { 
@@ -19,22 +20,26 @@ void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque); 
 void TratarFallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque); 
 void MostrarCACHE(T_CACHE_LINE *tbl);
-
+void lecturaArchivoBinario(char *nombreFichero, unsigned char *pSimul_RAM);
 
 
 int main(int argc, char **argv){
 
     int globalTime=0, numFallos=0;
     T_CACHE_LINE cache[NUM_FILAS];
+    unsigned char Simul_RAM[TAM_RAM];
+
     LimpiarCACHE(cache);
     MostrarCACHE(cache);
+
+    lecturaArchivoBinario("CONTENTS_RAM.bin", Simul_RAM);
     
 
 }
 
 
 void inicializarLinea(T_CACHE_LINE *linea){
-    linea->ETQ = 0xff;
+    linea->ETQ = 0xFF;
     for(int i=0; i<TAM_LINEA;i++){
         linea->Data[i] = 0x23;
     }
@@ -54,4 +59,23 @@ void MostrarCACHE(T_CACHE_LINE *tbl){
         printf("\n");
         
     }
+}
+
+void lecturaArchivoBinario(char *nombreFichero, unsigned char *pSimul_RAM){
+    FILE *arch = NULL;
+    char bitLeido;
+
+    arch = fopen(nombreFichero, "rb");
+    if (arch == NULL){
+        printf("Error: el fichero %s no existe", nombreFichero);
+        exit(1);
+    }
+        
+    while((bitLeido = getc(arch)) != EOF){
+        pSimul_RAM = bitLeido;
+        pSimul_RAM++;
+    }
+
+
+    fclose(arch);
 }
